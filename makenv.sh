55ef0fb59ff3b63c9a63dfac15b571aee918ba93
henriquefac <henriquefac@edu.unifor.br>
2025-03-27 11:52:48 -0300

    Melhorias no makenv.sh

--- makenv.sh
+++ makenv.sh
@@ -1,12 +1,26 @@
+#!/bin/bash
+
 echo "Iniciando projeto"
-if [ ! ".env"]; then
+
+# Verifica se o ambiente virtual existe
+if [ ! -d ".venv" ]; then
     echo "Criando ambiente virtual"
-    python3 -m venv .env
-    source env/bin/activate
-    pip -r requirements.txt
+    python3 -m venv .venv
+    source .venv/bin/activate
+    python3 -m pip install --upgrade pip
+    pip install -r requirements.txt
     deactivate
 fi
 
+# Ativa o ambiente virtual
 source start.sh
+
+
+if ! python -c "import requests" &> /dev/null; then
+    echo "Erro: requests não encontrado. Instalando..."
+    pip install requests
+fi
+
+# Executa os scripts
 python src/webscraping/utils_web/getlinks.py
 python src/webscraping/utils_web/download_files.py
