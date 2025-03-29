b6b94ee1739c4d4a03372a1b15d6fb8dc21cab9a
henriquefac <henriquefac@edu.unifor.br>
2025-03-29 19:55:28 -0300

    Melhorisas no config e criação do ambiente

--- config.py
+++ config.py
@@ -1,5 +1,26 @@
 from pathlib import Path
+from typing import Union
 
+
+# classe para gerenciar diretório e seus arquivos
+class DirManager():
+    def __init__(self, dir_path: Union[Path, "DirManager"]):
+        self.dir_path = Path(dir_path) if isinstance(dir_path, (str, Path)) else dir_path.dir_path
+    
+    def list_dir(self)->dict[str, 'DirManager']:
+        return {dir.name : DirManager(dir)  for dir in self.dir_path.iterdir() if dir.is_dir()}
+    
+    def list_file(self)->dict[str, Path]:
+        return {file.stem: file for file in self.dir_path.iterdir() if file.is_file()}
+
+    def __str__(self) -> str:
+        """Retorna o caminho como string ao chamar print(obj)."""
+        return str(self.dir_path)
+
+    def __repr__(self) -> str:
+        """Retorna a representação do objeto no modo debug."""
+        return f"DirManager({self.dir_path})"
+    
 class Config:
     BASE_PATH = Path(__file__).resolve().parent
     FILE_DIR_PATH = BASE_PATH / "files"
@@ -8,24 +29,17 @@ class Config:
 
     @classmethod
     def get_path_links(cls) -> dict[str, Path]:
-        return {file.name.split(".")[0]: file for file in cls.LINKS_DIR.iterdir() if file.is_file()}
+        return {file.stem: file for file in cls.LINKS_DIR.iterdir() if file.is_file()}
     
     @classmethod
-    def get_path_dir_data(cls) -> dict[str, Path]:
-        return {dir.name : dir  for dir in cls.DATA_PATH.iterdir() if dir.is_dir()}
-
-# classe para gerenciar diretório e seus arquivos
-class DirManager():
-    def __init__(self, dir_path: Path):
-        self.dir_path = dir_path
-    
-    def list_dir(self)->dict[str, Path]:
-        return {dir.name : dir  for dir in self.dir_path.iterdir() if dir.is_dir()}
-    def list_file(self)->dict[str, Path]:
-        return {file.name.split(".")[0]: file for file in self.dir_path.iterdir() if file.is_file()}
+    def get_path_dir_data(cls) -> dict[str, DirManager]:
+        return {dir.name : DirManager(dir)  for dir in cls.DATA_PATH.iterdir() if dir.is_dir()}
 
 
 if __name__ == "__main__":
-    print(Config.get_path_dir_data())
-    directory = DirManager(Config.get_path_dir_data()["microdados_enem_2023"])
-    print(directory.list_dir()) 
+    # Obtendo os diretórios dentro de DATA_PATH
+    data_dirs = Config.get_path_dir_data()
+    data_extract = data_dirs["microdados_enem_2023"]
+
+    print(data_extract.list_dir()["microdados_enem_2023_extract"])
+
--- makenv.sh
+++ makenv.sh
@@ -6,16 +6,34 @@ echo "Iniciando projeto"
 if [ ! -d ".venv" ]; then
     echo "Criando ambiente virtual"
     python3 -m venv .venv
+
+    # Define as variáveis de ambiente para o ambiente virtual
+    echo "export PYTHONPATH=$(pwd)" >> .venv/bin/activate
+    echo "export JUPYTER_PATH=$(pwd)" >> .venv/bin/activate
+
+    # Ativa o ambiente virtual
     source .venv/bin/activate
+    
+    # Atualiza o pip e instala dependências
     python3 -m pip install --upgrade pip
-    pip install -r requirements.txt
+
+    # Verifica se requirements.txt existe antes de instalar
+    if [ -f "requirements.txt" ]; then
+        pip install -r requirements.txt
+    else
+        echo "Aviso: requirements.txt não encontrado. Nenhum pacote foi instalado."
+    fi
+
+    # criar kernel
+    python -m ipykernel install --user --name=ciencia_de_dados_av1 --display-name "Python (ciencia_de_dados_av1)"
+
     deactivate
 fi
 
 # Ativa o ambiente virtual
-source start.sh
+source .venv/bin/activate
 
-
+# Verifica se 'requests' está instalado corretamente
 if ! python -c "import requests" &> /dev/null; then
     echo "Erro: requests não encontrado. Instalando..."
     pip install requests
